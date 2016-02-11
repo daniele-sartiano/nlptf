532081c6e2c159b20ca9a84242aba801afcf27a3
Daniele Sartiano <daniele.sartiano@gmail.com>
2016-02-11 13:59:03 +0100

    saving vocabulary and labels indexes

--- nlptf/tests/ner.py
+++ nlptf/tests/ner.py
@@ -4,8 +4,10 @@ import unittest
 import sys
 import os
 
+import numpy as np
+import cPickle as pickle
+
 from nlptf.reader import IOBReader
-from nlptf.trainer import NerTrainer
 from nlptf.models.linear import LinearClassifier
 
 class TestReader(unittest.TestCase):
@@ -22,6 +24,9 @@ class TestLinear(unittest.TestCase):
     def test_train(self):
         reader = IOBReader(sys.stdin)
         X, y = reader.read()
+
+        pickle.dump([reader.vocabulary, reader.labels_idx], open('reader.pkl', 'wb'))
+                
         dataset = []
         labels = []
 
@@ -40,9 +45,38 @@ class TestLinear(unittest.TestCase):
         c = LinearClassifier()
         path = c.train(train_dataset, train_labels,
                        dev_dataset, dev_labels)
+        
 
+    def test_predict(self):
+        vocab, labels_idx = pickle.load(open('reader.pkl'))
+        
+        lines = sys.stdin.readlines()
 
-    # def test_predict(self):
-    #     reader = IOBReader(sys.stdin)
-    #     trainer = NerTrainer(reader, LinearClassifier())
-    #     trainer.train()
+        reader = IOBReader(lines, vocabulary=vocab, labels_idx=labels_idx)
+        X, _ = reader.read()
+
+        dataset = []
+
+        for i, sentence in enumerate(X):
+            for ii, token in enumerate(sentence):
+                dataset.append(token)
+        
+        c = LinearClassifier()
+        predicted = c.predict(dataset)
+        self.assertEqual(len(dataset), len(predicted))
+
+        labels_list = [None]* len(labels_idx)
+        for k, v in labels_idx.items():
+            labels_list[np.argmax([v], 1)[0]] = k
+
+        print labels_list
+
+        i = 0
+        for line in lines:
+            line = line.strip()
+            if line:                
+                print '%s\t%s' % (line.split('\t')[0], labels_list[predicted[i]])
+                i += 1
+            else:
+                print
+        
